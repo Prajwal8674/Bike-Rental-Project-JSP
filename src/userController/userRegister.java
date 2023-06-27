57ef288a96f8d17b1cd56833b59689e532b13b8c
TLS-Escanor <patilprajwal8804@gmail.com>
2023-06-28 01:29:54 +0530

    added validations

--- src/userController/userRegister.java
+++ src/userController/userRegister.java
@@ -58,34 +58,34 @@ try {
 	}
 	else if(pass!=cupass)
 	{
-//		request.getRequestDispatcher("userRegister.jsp").include(request, response);
-//		out.print("<!DOCTYPE html>");
-//		out.print("<html>");
-//		out.print("<head>");
-//		out.print("<meta charset='0ISO-8859-1'>");
-//		out.print("<title>Insert title here</title>");
-//		out.print("<style>");
-//		out.print("p{");
-//		out.print("background-color: red;");
-//		out.print("text-align:center;");
-//		out.print("color:white;");
-//		out.print("width:60%;");
-//		out.print("}");
-//		out.print("h2{");
-//		out.print("color :red;");
-//		out.print("text-align:center;");
-//		out.print("width:60%;");
-//		out.print("display: flex;");
-//		out.print("position:relative;");
-//		out.print("top : 20px;");
-//		out.print("}");
-//		out.print("</style>");
-//		out.print("</head>");
-//		out.print("<body>");
-//		out.print("<marquee scrollamount='15'><h2><p> ! Both Psswords are Different</p></h2></marquee>");
-//		out.print("</body>");
-//		out.print("</html");
-//	
+		request.getRequestDispatcher("userRegister.jsp").include(request, response);
+		out.print("<!DOCTYPE html>");
+		out.print("<html>");
+		out.print("<head>");
+		out.print("<meta charset='0ISO-8859-1'>");
+		out.print("<title>Insert title here</title>");
+		out.print("<style>");
+		out.print("p{");
+		out.print("background-color: red;");
+		out.print("text-align:center;");
+		out.print("color:white;");
+		out.print("width:60%;");
+		out.print("}");
+		out.print("h2{");
+		out.print("color :red;");
+		out.print("text-align:center;");
+		out.print("width:60%;");
+		out.print("display: flex;");
+		out.print("position:relative;");
+		out.print("top : 20px;");
+		out.print("}");
+		out.print("</style>");
+		out.print("</head>");
+		out.print("<body>");
+		out.print("<marquee scrollamount='15'><h2><p> ! Both Psswords are Different</p></h2></marquee>");
+		out.print("</body>");
+		out.print("</html");
+	
 	}
 	}
 	else if(ud.checkUser(email)){
